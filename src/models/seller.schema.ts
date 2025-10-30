b85f96d0c07a9fc49a5cf203eefb4d72b4454f78
Keith <karirukeith@gmail.com>
2025-10-30 11:51:03 +0300

    updated shared library to latest version

--- src/models/seller.schema.ts
+++ src/models/seller.schema.ts
@@ -59,7 +59,7 @@ const sellerSchema: Schema = new Schema(
         canceledJobs: {type: Number, default: 0},
         totalEarnings: {type: Number, default: 0},
         totalGigs: {type: Number, default: 0},
-        createdAt: {type: Date, default: Date.now()},
+        createdAt: {type: Date, default: Date.now},
     },
     {
         versionKey: false
